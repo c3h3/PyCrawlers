c69f0000938e758dcf55f784edf9268a2de1b178
DboyLiao <DboyLiao@DboyLiaodeMacBook-Pro.local>
2014-09-26 17:18:57 +0800

    All video urls loaded.

---
+++ startMongod.sh
@@ -0,0 +1,1 @@
+mongod --port 27100 --dbpath /Users/DboyLiao/Documents/Python_Learning/PyCrawlers/TWusRDB/db/ --logpath /Users/DboyLiao/Documents/Python_Learning/PyCrawlers/TWusRDB/log/db.log --oplogSize 50  --fork
